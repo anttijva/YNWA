d6062630d1004150d152764129c8e0fd0352ac10
iosdev <iosdev@mtk-idev-mba-15.local>
2016-04-07 14:40:05 +0300

    storyboad and data model

---
+++ YNWAModel/PersonTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonTableViewCell.swift
+//  YNWAModel
+//
+//  Created by iosdev on 7.4.2016.
+//  Copyright © 2016 thanhbinhtran. All rights reserved.
+//
+
+import Cocoa
+
+class PersonTableViewCell: UITableViewCell {
+
+}
